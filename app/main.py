f071d8c1f30fc5bede1fec44ebdf8daa7bdef519
noshenko98 <noshenko98@gmail.com>
2026-02-11 16:43:38 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+import shutil
+import os
+
+
+def move_file(command: str) -> None:
+    command = command.split()
+    if command[0] == "mv" and len(command) == 3:
+        print(command)
+        path = command[2].split("/")
+        path_start = ""
+        for part_patch in path[:-1]:
+            try:
+                os.mkdir(path_start + part_patch)
+            except FileExistsError:
+                pass
+            finally:
+                path_start += part_patch + "/"
+        shutil.copy2(command[1], command[2])
+        os.remove(command[1])
